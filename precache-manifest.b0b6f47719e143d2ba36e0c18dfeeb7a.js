b47ec7dda647229d77894af05b7d78622ddd6b5f
Anthon_M <xll2005@tut.by>
2020-03-17 22:07:15 +0300

    feat: add project

---
+++ precache-manifest.b0b6f47719e143d2ba36e0c18dfeeb7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb0fd7e7fa4832ea98362e61da00c18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c4f7d1c83d951ee64a8",
+    "url": "/static/css/main.555c7c9b.chunk.css"
+  },
+  {
+    "revision": "9c4f59e08ac671b74d8d",
+    "url": "/static/js/2.f965736b.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.f965736b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c4f7d1c83d951ee64a8",
+    "url": "/static/js/main.c97c2d14.chunk.js"
+  },
+  {
+    "revision": "707d41e56b4e984fb3b1",
+    "url": "/static/js/runtime-main.335ad2b1.js"
+  },
+  {
+    "revision": "b5e00e04e8ca1ef91662a3ab77432f2f",
+    "url": "/static/media/sort-asc.b5e00e04.svg"
+  },
+  {
+    "revision": "391ccdd8c7136b4be1cd99829b1b71f1",
+    "url": "/static/media/sort-desc.391ccdd8.svg"
+  },
+  {
+    "revision": "a455d9bdba106f0001b1797eaff98264",
+    "url": "/static/media/sort-not.a455d9bd.svg"
+  }
+]);
